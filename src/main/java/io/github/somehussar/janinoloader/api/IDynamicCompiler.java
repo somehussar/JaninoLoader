ce92d6e63ca7851ab4e857bab3c8a7c3d41749b5
somehussar <148253652+somehussar@users.noreply.github.com>
2025-12-03 04:06:20 +0100

    Better documentation.

--- src/main/java/io/github/somehussar/janinoloader/api/IDynamicCompiler.java
+++ src/main/java/io/github/somehussar/janinoloader/api/IDynamicCompiler.java
@@ -4,7 +4,6 @@ import org.codehaus.commons.compiler.CompileException;
 import org.codehaus.commons.compiler.util.resource.Resource;
 import org.codehaus.commons.compiler.util.resource.StringResource;
 
-import java.io.File;
 import java.io.IOException;
 
 public interface IDynamicCompiler {
@@ -14,9 +13,10 @@ public interface IDynamicCompiler {
      * <br>
      * Supplying multiple resources at once allows for circular-dependency in compiled classes. <br>
      * <br>
-     * {@link Resource} filename must either be a file-path format (<code>my/project/VeryCoolClass.java</code>) or fully qualified format (<code>my.project.VeryCoolClass</code>) <br>
+     * {@link Resource} filename <bold>MUST</bold> be either a file-path format (<code>my/project/VeryCoolClass.java</code>) or fully qualified format (<code>my.project.VeryCoolClass</code>) <br>
+     * If filename doesn't follow a specific format, neither {@linkplain IDynamicCompiler#recompileClass(Resource...) class hotswapping} nor {@linkplain #removeClass(String...) class removal} is possible. <br>
      * <br>
-     * Suggested to use {@link StringResource} however any other resource is fine as well as long as it follows former specification.
+     * Suggested to use {@link StringResource} however any other resource is fine as well as long as it follows former specification. <br>
      * @param resources List of resources
      * @throws CompileException Error given during compilation
      */
@@ -43,7 +43,7 @@ public interface IDynamicCompiler {
      * An example of when it will be called: 
      * <ul>
      *     <li>A class has been removed from the class loader.</li>
-     *     <li>A class has been reloaded.</li>
+     *     <li>A class has been recompiled.</li>
      * </ul>
      * @param listeners Listener(s) to add
      */
