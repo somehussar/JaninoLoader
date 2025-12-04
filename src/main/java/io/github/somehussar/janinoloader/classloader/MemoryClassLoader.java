ef8f39595d3f5799f90081e11c02519648e28340
somehussar <148253652+somehussar@users.noreply.github.com>
2025-12-04 13:37:46 +0100

    refactor names for debugging

--- src/main/java/io/github/somehussar/janinoloader/classloader/MemoryClassLoader.java
+++ src/main/java/io/github/somehussar/janinoloader/classloader/MemoryClassLoader.java
@@ -6,21 +6,19 @@ import io.github.somehussar.janinoloader.api.delegates.LoadClassCondition;
 import java.util.Map;
 
 public class MemoryClassLoader extends FilteredClassLoader {
-    private final Map<String, byte[]> classes;
+    private final Map<String, byte[]> storedClasses;
 
-    public MemoryClassLoader(ClassLoader parent, LoadClassCondition classFilter, Map<String,byte[]> classes) {
+    public MemoryClassLoader(ClassLoader parent, LoadClassCondition classFilter, Map<String,byte[]> storedClasses) {
         super(parent, classFilter);
-        this.classes = classes;
+        this.storedClasses = storedClasses;
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        byte[] bytes = classes.get(name.replaceAll("\\.", "/") + ".class");
+        byte[] bytes = storedClasses.get(name.replaceAll("\\.", "/") + ".class");
         if (bytes != null) {
             return defineClass(name, bytes, 0, bytes.length);
         }
-        if (name.equals("API")) name = "java.lang.Math";
         return super.findClass(name);
     }
-
 }
