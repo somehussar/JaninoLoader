640bae533264ef31cff67cab4e35475c1b6d56bc
somehussar <148253652+somehussar@users.noreply.github.com>
2025-12-04 17:23:30 +0100

    Document

--- src/test/java/io/github/somehussar/janinoloader/example/AbstractNPCScript.java
+++ src/test/java/io/github/somehussar/janinoloader/example/AbstractNPCScript.java
@@ -4,6 +4,7 @@ public abstract class AbstractNPCScript {
     // If using Serializable
     // (for example, when wanting to preserve script-defined variable states)
     // Mark these fields as transient so they don't get picked up.
+    // marked private as to not allow scripts to edit it.
     private transient Npc npc;
 
     public static AbstractNPCScript reloadHandler(AbstractNPCScript old, AbstractNPCScript newObj) {
